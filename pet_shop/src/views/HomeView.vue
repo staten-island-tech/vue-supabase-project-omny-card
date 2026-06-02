af93eed6b238104eccc182752734169d036098bd
yanyinnicolewu <nicolewu288@gmail.com>
2026-06-02 12:31:26 -0400

    bmnnbm

--- pet_shop/src/views/HomeView.vue
+++ pet_shop/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Pls log in to your acc here</h1>
+  </div>
 </template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
