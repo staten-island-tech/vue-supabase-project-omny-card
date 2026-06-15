7af18a548dfa5dad79097ad1bb5a6138c948e45b
yanyinnicolewu <nicolewu288@gmail.com>
2026-06-15 00:20:35 -0400

    Update App.vue

--- pet_shop/src/App.vue
+++ pet_shop/src/App.vue
@@ -2,7 +2,6 @@
   <div :class="theme">
     <nav>
       <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/login">Login</RouterLink>
     </nav>
 
